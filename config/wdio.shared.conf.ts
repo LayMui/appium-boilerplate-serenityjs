d38cfb6520982f5682abda8615ade32b9be3d2ad
LayMui <laymui.toh@yara.com>
2022-03-16 17:03:34 +0800

    Remove wdio-cucumberjs-json-reporter

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -128,8 +128,7 @@ export const config: WebdriverIOConfig = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'
-    ],
+    reporters: ['spec'],
     // Options to be passed to Mocha.
     mochaOpts: {
         ui: "bdd",
